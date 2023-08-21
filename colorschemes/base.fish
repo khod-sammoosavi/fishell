25efc30433e1bf68bfe0afe89028289cf8424682
SamMoosavi <hi.sammoosavi@gmail.com>
2023-08-22 00:22:36 +0330

    initialize commit

---
+++ colorschemes/base.fish
@@ -0,0 +1,52 @@
+set -g cyan (set_color -o white)
+set -g yellow (set_color -o white)
+set -g red (set_color -o white)
+set -g blue (set_color -o white)
+set -g green (set_color -o white)
+set -g purple (set_color -o white)
+set -g normal (set_color white)
+
+set -g whitespace " "
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set __fish_git_prompt_showdirtystate "yes"
+set __fish_git_prompt_showstashstate "yes"
+set __fish_git_prompt_showuntrackedfiles "yes"
+set __fish_git_prompt_showupstream "yes"
+set __fish_git_prompt_color_branch purple
+set __fish_git_prompt_color red
+set __fish_git_prompt_char_stateseparator ""
+set __fish_git_prompt_char_dirtystate "U"
+set __fish_git_prompt_char_stagedstate "S"
+set __fish_git_prompt_char_untrackedfiles "!"
+set __fish_git_prompt_char_stashstate "B"
+set __fish_git_prompt_char_upstream_ahead "!"
+set __fish_git_prompt_char_upstream_behind "U"
+set __fish_git_prompt_char_cleanstate ""
+set __fish_git_prompt_color_prefix blue
+set __fish_git_prompt_color_suffix blue
+
+function fish_prompt
+set last_status $status
+export VIRTUAL_ENV_DISABLE_PROMPT=1
+
+if test -e package.json
+set node_prompt $green " ⬢ (node)"
+end
+
+if test $last_status = 0
+set initial_indicator $blue"[$normal$USER$red@$normal$hostname$blue]"
+set status_indicator "$normal❯$cyan❯$green❯$purple❯"
+else
+set initial_indicator $blue"[$normal$USER$red@$normal$hostname$blue]"
+set status_indicator "$red❯$red❯$red❯$red❯"
+end
+if set -q VIRTUAL_ENV
+set venv $whitespace (set_color yellow) " (" (basename "$VIRTUAL_ENV") (set_color yellow) ")"
+end
+
+printf "%s%s%s%s%s%s%s%s%s" $initial_indicator $whitespace $cyan(basename (prompt_pwd)) $(__fish_git_prompt) $venv $node_prompt $whitespace $status_indicator $whitespace
+
+end
---
+++ config.fish
@@ -0,0 +1,49 @@
+if status is-interactive
+  function new_alias
+    alias $argv
+  end
+  # load user config file
+  source $HOME/.fishrc.fish
+  # set welcome message
+  set fish_greeting $welcome_message
+  # term color
+  set -gx TERM xterm-256color
+  # pager
+  set -gx PAGER less
+  # theme
+  set -g theme_color_scheme terminal-dark
+  set -g fish_prompt_pwd_dir_length 1
+  set -g theme_display_user yes
+  set -g theme_hide_hostname no
+  set -g theme_hostname always
+  # aliases
+  new_alias ls "exa --icons"
+  new_alias ll "exa -l -g -a --icons"
+  new_alias lt "exa --tree --icons -l -g"
+  new_alias mirror "sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+  new_alias mirrord "sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
+  new_alias mirrors "sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
+  new_alias mirrora "sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+  new_alias grep "grep --color=always"
+  new_alias egrep "egrep --color=always"
+  new_alias cp "cp -i"
+  new_alias mv "mv -i"
+  new_alias rm "rm -i"
+  new_alias disk "df -h"
+  new_alias sysctl "systemctl list-unit-files --state=enabled"
+  # default editor
+  set -gx EDITOR $editor
+  # add paths
+  set -gx PATH bin $PATH
+  set -gx PATH $HOME/bin $PATH
+  set -gx PATH $HOME/.local/bin $PATH
+  set -gx PATH "$HOME/.cargo/bin:$PATH"
+  set -gx PATH $PATH
+  # nodejs
+  set -gx PATH node_modules/.bin $PATH
+  # go
+  set -g GOPATH $HOME/go
+  set -gx PATH $GOPATH/bin $PATH
+  # prompt
+  source (dirname (status --current-filename))/colorschemes/$colorscheme.fish
+end
