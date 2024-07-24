130aa912b97118bf069dc715ef9251adc7705ea1
SamMoosavi <hi.sammoosavi@gmail.com>
2024-07-24 09:36:00 +0330

    feat(colorscheme): remove colorschemes.

--- example-fishrc.fish
+++ example-fishrc.fish
@@ -1,6 +1,6 @@
-set welcome_message ""
+set welcome_message "$(lastlog -u $USER)[2]"
 set editor "dvim"
-set colorscheme "default"
+set colorscheme "base"
 
 # set new alias
 new_alias extree "exa --tree --icons"
